0a3e8172bae1bdd7e6135ba09f9b2cf8b799e572
Sriniket Sahoo <sriniket.sahoo@iserveu.co.in>
2026-04-09 11:25:53 +0530

    add: authentic flow

---
+++ src/auth/authconfig.ts
@@ -0,0 +1,8 @@
+import type { UserManagerSettings } from "oidc-client-ts";
+
+export const authConfig : UserManagerSettings= {
+  authority: 'https://pnb-auth-stage.isupay.in/application/o/pnb/',
+  client_id: 'SaDG8kozoNOUC07Uv46et8',
+  redirect_uri: 'http://localhost:3000/redirected',
+  scope: "path openid profile email offline_access authorities privileges user_name created adminName bankCode goauthentik.io/api"
+};
